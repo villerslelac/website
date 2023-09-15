e4ea819254655e0f4a2389a47830c4d7b04c6676
Romain VERMOT <romain.vermot@posteo.net>
2023-09-15 23:29:21 +0000

    Add the main menu

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,95 +1,106 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import { useState } from 'react';
+import styles from './page.module.scss'
+import clsx from 'clsx';
+
+const items = [
+  'Votre Mairie',
+  'Vos démarches',
+  'Vivre à Villers',
+  'Découvrir Villers-le-Lac'
+];
+
+const subItems = [
+  'Équipe municipale',
+  'Employés municipaux',
+  'Organigramme des services',
+  'Services',
+  'Séances du Conseil municipal',
+  'Bulletins municipaux',
+  'Marchés publics',
+  'Offres d\'emploi'
+];
 
 export default function Home() {
+  const [activeItem, setActiveItem] = useState<number | null>(null);
+  const [currentPos, setCurrentPos] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    if (!open) {
+      console.log(window.scrollY);
+      setCurrentPos(window.scrollY);
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+      setActiveItem(null);
+    }
+    setOpen(!open)
+  }
+
+  const toggleItem = (idx: number) => {
+    if (activeItem != idx) {
+      setActiveItem(idx);
+    } else {
+      setActiveItem(null);
+    }
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header style={{ '--scroll-top': `-${currentPos}px` }}>
+        <nav className={clsx(open && styles.open, styles.navbar)}>
+          <div className={styles.menu}>
+            <a href="/" className={styles.logo}></a>
+            <button
+              className={styles.btn}
+              onClick={toggleMenu}
+            >
+              <span className={styles.btnBurger} />
+            </button>
+          </div>
+          <ul className={styles.items}>
+            {items.map((item, idx) => {
+              if (idx == 0) {
+                return (
+                  <li key={idx} className={clsx(idx == activeItem && styles.open, styles.expandableItem)}>
+                    <a href="javascript:void(0)" onClick={(e) => { e.stopPropagation(); toggleItem(idx) }}>{item}</a>
+                    <div className={styles.submenu}>
+                      <div className={styles.submenuContent}>
+                        <span className={styles.submenuTitle}>{item}</span>
+                        <a href="#" className={styles.submenuSeeMore}>Voir toute la rubrique</a>
+                        <ul>
+                          {subItems.map((item, idx) => (
+                            <li key={idx}>
+                              <a href="#">{item}</a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </li>
+                );
+              }
+              return (
+                <li key={idx}>
+                  <a href="#">{item}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <div className={styles.headband}>
+          <h1>Mon titre</h1>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </header>
+      <main className={styles.main}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu venenatis diam, in porta tortor. Donec cursus, tortor vel consectetur euismod, dui orci dignissim justo, sit amet fermentum erat magna id neque. In interdum metus tellus, id volutpat ante consectetur ut. Phasellus accumsan ut leo et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed libero orci, aliquam nec efficitur ac, auctor in ex. Maecenas sodales dictum sapien vel mattis. Nulla vitae auctor urna. Sed dignissim quam augue, quis viverra massa posuere quis. Proin tortor mauris, porttitor quis commodo at, suscipit et libero. Nullam turpis odio, ullamcorper et tempor id, mollis in turpis. Donec facilisis lorem interdum fermentum aliquet.
+        </p>
+        <p>
+          Fusce maximus eu metus quis porta. Nulla facilisi. Ut faucibus interdum nisl et accumsan. Proin rutrum felis neque, in varius ante malesuada auctor. Maecenas id quam urna. Mauris at posuere enim, quis venenatis turpis. Pellentesque justo sapien, finibus sed ullamcorper ac, tincidunt ut lectus. Duis eu felis blandit, blandit ex feugiat, aliquet lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas feugiat gravida pellentesque.
+        </p>
+      </main>
+    </>
   )
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  webpack: (config) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object').oneOf.filter((rule) => Array.isArray(rule.use));
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (moduleLoader.loader !== undefined  &&
+          moduleLoader.loader.includes('css-loader') &&
+          typeof moduleLoader.options.modules === 'object'
+        ) {
+          moduleLoader.options = {
+            ...moduleLoader.options,
+            modules: {
+              ...moduleLoader.options.modules,
+              // This is where we allow camelCase class names
+              exportLocalsConvention: 'camelCase'
+            }
+          };
+        }
+      });
+    });
+    return config;
+  },
+}
 
 module.exports = nextConfig
